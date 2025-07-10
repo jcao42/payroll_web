b32edd4a6e4d19d1eddae33cac8c52407d8460a5
Jason <classievals@gmail.com>
2025-07-10 00:10:31 -0400

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -37,5 +37,8 @@ def index():
 
     return render_template("index.html", employees=employees, entry_map=entry_map, today=today)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
